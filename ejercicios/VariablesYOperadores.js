da9c67508197b615904f2c0184de3f5a2d44c849
SantiMaujo <santimaujo@gmail.com>
2024-06-04 10:06:08 -0300

    Primer ejercicio

---
+++ ejercicios/VariablesYOperadores.js
@@ -0,0 +1,10 @@
+//Ejercicio a)
+// Declaración de dos variables numéricas
+let num1 = 15;
+let num2 = 35;
+
+// Sumar las dos variables y guardar el resultado en una tercera variable
+let suma = num1 + num2;
+
+// Imprimir el resultado
+console.log("La suma de num1 y num2 es: " + suma);
